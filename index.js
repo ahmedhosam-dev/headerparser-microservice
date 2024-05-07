fa5a6696f3d37ca8d9b2913bc4aa5f430cdf1b9f
ahmedhosam-dev <ahmed.hossam.t.11@gmail.com>
2024-05-07 13:23:04 +0300

    Add whoami api request

--- index.js
+++ index.js
@@ -3,12 +3,16 @@
 
 // init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
+
+const requestIp = require('request-ip');
+app.use(requestIp.mw());
+
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-var cors = require('cors');
+let cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -22,6 +26,14 @@ app.get('/', function (req, res) {
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.get('/api/whoami', (req, res) => {
+  res.json({
+    "ipaddress": req.clientIp,
+    "language": req.acceptsLanguages(),
+    "software": req.get("User-Agent")
+  });
 });
 
 // listen for requests :)
